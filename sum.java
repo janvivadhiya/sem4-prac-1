250dac9cf61f00287f92e7f3366a3dd4fa17925c
janvivadhiya <141604821+janvivadhiya@users.noreply.github.com>
2024-02-01 19:16:03 -0800

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,16 @@
+class sum
+{
+	public static void main(String s[])
+	{
+		 int a = 5;
+        	 int b = 10;
+        	 int c = 15;
+
+		int sum = a + b + c;
+
+        double average = (double) sum / 3;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+	}
+}
